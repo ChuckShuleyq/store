448234265147f8d0d94979c9a5cfcf75fefe7ad6
Nikita Melnikov <nikitamelnikov502@gmail.com>
2023-07-28 17:44:57 +0500

    new file:   media/users/__init__.py 	new file:   media/users/__pycache__/__init__.cpython-310.pyc 	new file:   media/users/__pycache__/admin.cpython-310.pyc 	new file:   media/users/__pycache__/apps.cpython-310.pyc 	new file:   media/users/__pycache__/forms.cpython-310.pyc 	new file:   media/users/__pycache__/models.cpython-310.pyc 	new file:   media/users/__pycache__/urls.cpython-310.pyc 	new file:   media/users/__pycache__/views.cpython-310.pyc 	new file:   media/users/admin.py 	new file:   media/users/apps.py 	new file:   media/users/forms.py 	new file:   media/users/migrations/0001_initial.py 	new file:   media/users/migrations/__init__.py 	new file:   media/users/migrations/__pycache__/0001_initial.cpython-310.pyc 	new file:   media/users/migrations/__pycache__/__init__.cpython-310.pyc 	new file:   media/users/models.py 	new file:   media/users/templates/users/base.html 	new file:   media/users/templates/users/email_verification.html 	new file:   media/users/templates/users/login.html 	new file:   media/users/templates/users/profile.html 	new file:   media/users/templates/users/register.html 	new file:   media/users/tests.py 	new file:   media/users/urls.py 	new file:   media/users/views.py 	new file:   "media/users_images/230px-\320\237\320\276\321\201\321\202\320\265\321\200_\321\204\320\270\320\273\321\214\320\274\320\260_\320\257-\320\273\320\265\320\263\320\265\320\275\320\264\320\260.jpg" 	new file:   pr.sqlite3 	modified:   product/__pycache__/__init__.cpython-310.pyc 	modified:   product/__pycache__/admin.cpython-310.pyc 	modified:   product/__pycache__/apps.cpython-310.pyc 	modified:   product/__pycache__/models.cpython-310.pyc 	modified:   product/__pycache__/urls.cpython-310.pyc 	modified:   product/__pycache__/views.cpython-310.pyc 	modified:   product/migrations/__pycache__/0001_initial.cpython-310.pyc 	modified:   product/migrations/__pycache__/__init__.cpython-310.pyc 	modified:   store/__pycache__/__init__.cpython-310.pyc 	modified:   store/__pycache__/settings.cpython-310.pyc 	modified:   store/__pycache__/urls.cpython-310.pyc 	modified:   store/__pycache__/wsgi.cpython-310.pyc 	new file:   store/settings.py 	modified:   users/__pycache__/__init__.cpython-310.pyc 	modified:   users/__pycache__/admin.cpython-310.pyc 	modified:   users/__pycache__/apps.cpython-310.pyc 	modified:   users/__pycache__/forms.cpython-310.pyc 	modified:   users/__pycache__/models.cpython-310.pyc 	modified:   users/__pycache__/urls.cpython-310.pyc 	modified:   users/__pycache__/views.cpython-310.pyc 	modified:   users/forms.py 	modified:   users/migrations/0001_initial.py 	modified:   users/migrations/__pycache__/0001_initial.cpython-310.pyc 	modified:   users/migrations/__pycache__/__init__.cpython-310.pyc 	modified:   users/models.py 	modified:   users/templates/users/profile.html 	modified:   users/views.py

---
+++ media/users/urls.py
@@ -0,0 +1,8 @@
+# from django.urls import path 
+# from users.views import register_page, login_page, profile
+# urlpatterns = [
+#     path('register.html', register_page, name="register"),
+#     path('login.html', login_page, name="login"),
+#     path('profile.html', profile, name="profile"),
+# ]
+# app_name = 'users'
--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, HttpResponseRedirect
+from django.http import HttpRequest
 from users.models import User
 from users.forms import UserLoginForm, UserRegisterForm, UserProfileForm
 from django.contrib.auth import authenticate, login
 from django.urls import reverse
 # Create your views here.
-def register_page(request):
+def register_page(request: HttpRequest):
     if request.method == 'POST':
         form = UserRegisterForm(data=request.POST) 
         if form.is_valid():
@@ -15,7 +16,7 @@ def register_page(request):
     context = {'form': form, 'title': 'Store - Регистрация'}
     return render(request, 'users/register.html', context=context)
 
-def login_page(request):
+def login_page(request: HttpRequest):
     if request.method == 'POST':
         form = UserLoginForm(data=request.POST)
         if form.is_valid():
@@ -30,16 +31,20 @@ def login_page(request):
     context = {'form': UserLoginForm(), 'title': 'Store - вход'}
     return render(request, 'users/login.html', context)
 
-def profile(request):
-    if request.method == 'POST':
-        form = UserProfileForm(data=request.user)
-        if form.is_valid():
-            form.save()
-    else:
-        form = UserProfileForm()
+def profile(request: HttpRequest):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+            if form.is_valid():
+                form.save()
+                return HttpResponseRedirect(reverse('users:profile'))
+        else:
+            form = UserProfileForm(instance=request.user)
 
-    context = {
-        'title': 'Store - профиль',
-        'form': form,
-    }
-    return render(request, 'users/profile.html', context)
+        context = {
+            'title': 'Store - профиль',
+            'form': form,
+        }
+        return render(request, 'users/profile.html', context)
+    else:
+        raise PermissionError("You don't have the right to view this page!")

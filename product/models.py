c043ee061e575939e135b5524fb000a47fa238c2
Nikita Melnikov <nikitamelnikov502@gmail.com>
2023-08-01 12:20:21 +0500

    modified:   product/models.py

--- product/models.py
+++ product/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from users.models import User
+
 # Create your models here.
 class ProductCategory(models.Model):
     name = models.CharField(max_length=64)
@@ -12,3 +14,9 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to="product_images")
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+class Basket(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
